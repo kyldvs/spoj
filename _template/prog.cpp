fc8e5209e838bc85c6ad10d7ef40a06a4481255d
Kyle Davis <kad@fb.com>
2016-08-10 00:24:28 -0700

    update template

--- _template/prog.cpp
+++ _template/prog.cpp
@@ -1,10 +1,15 @@
 
 #include <climits>
 #include <iostream>
+#include <string>
 #include <vector>
+
+#define DEBUG if (true) printf
+
+using namespace std;
 
 int main() {
   int x;
-  std::cin >> x;
+  cin >> x;
   printf("%d\n", x);
 }
